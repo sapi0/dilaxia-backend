a7f5d90f64481d6fb95d386ba25cfb5ddea28e8a
jack <giacomoarbia@gmail.com>
2024-04-23 13:13:49 +0200

    [DA-64] creazione subscriptions e eventLight

---
+++ src/main/java/com/sapi0/dilaxiabackend/data/dto/SubscriptionsDTO.java
@@ -0,0 +1,11 @@
+package com.sapi0.dilaxiabackend.data.dto;
+
+import java.util.List;
+
+public class SubscriptionsDTO {
+
+    public List<EventUserDTO> queue;
+    public List<EventUserDTO> subscriptions;
+
+
+}
