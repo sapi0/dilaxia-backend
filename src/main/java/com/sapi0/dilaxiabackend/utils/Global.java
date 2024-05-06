de76ca3daeb601f017e441a83ca895d7a5c4a127
Crih.exe <36051144+Crihexe@users.noreply.github.com>
2024-05-06 23:44:25 +0200

    [FIX] hotfix. email regex fixed

--- src/main/java/com/sapi0/dilaxiabackend/utils/Global.java
+++ src/main/java/com/sapi0/dilaxiabackend/utils/Global.java
@@ -14,7 +14,7 @@ public class Global {
     public static final int USER_TYPE_EXTERNAL = 0;
 
     public static final Pattern REGEX_LETTERS_SPACES = Pattern.compile("^[ A-Za-z]+$");
-    public static final Pattern REGEX_EMAIL = Pattern.compile("\\w+([-+.']\\w+)@\\w+([-.]\\w+).\\w+([-.]\\w+)*");
+    public static final Pattern REGEX_EMAIL = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`\\{|\\}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`\\{|\\}~-]+)*|\\\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\\\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
     public static final Pattern REGEX_PASSWORD = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&!?\\-,./*_+=])(?=\\S+$).{8,}$");
 
 }
